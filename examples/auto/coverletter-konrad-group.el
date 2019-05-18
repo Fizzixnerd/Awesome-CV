4138d53fdf544a46a9544a10bd82dd20743062ac
Matt Walker <matt.g.d.walker@gmail.com>
2019-05-17 21:34:33 -0400

    Update resumes

---
+++ examples/auto/coverletter-konrad-group.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook
+ "coverletter-konrad-group"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("awesome-cv" "11pt" "a4paper")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "awesome-cv"
+    "awesome-cv11"
+    "color"))
+ :latex)
+
